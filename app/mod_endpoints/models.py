227006ed51cff6e13d8af9b7f3607be7751bb8ad
Osioke Itseuwa <osiokeitseuwa@gmail.com>
2015-11-18 20:17:36 +0100

    Add endpoint to get one state

--- app/mod_endpoints/models.py
+++ app/mod_endpoints/models.py
@@ -2,16 +2,19 @@ from parse_rest.datatypes import Object
 
 class State(Object):
 
-  def as_dict(self):
-    state = {}
-    state['name'] = self.name
-    state['latitude'] = self.latitude
-    state['longitude'] = self.longitude
-    state['min-lat'] = self.min_latitude
-    state['min-long'] = self.min_longitude
-    state['max-lat'] = self.max_latitude
-    state['max-long'] = self.max_longitude
-    return state
+    def as_dict(self):
+        state = {}
+        state['name'] = self.name
+        state['capital'] = self.cap_city
+        state['latitude'] = self.latitude
+        state['longitude'] = self.longitude
+        state['min-lat'] = self.min_latitude
+        state['min-long'] = self.min_longitude
+        state['max-lat'] = self.max_latitude
+        state['max-long'] = self.max_longitude
+        state['state-code'] = self.state_code
+        state['state-id'] = self.state_id
+        return state
 
   @staticmethod
   def get_all_states():
@@ -21,4 +24,25 @@ class State(Object):
       result_set.append(state.as_dict())
     return result_set
 
+@staticmethod
+def get_one_state(state_name):
+    result_set = []
+    state = State.Query.get(name = state_name).limit(1)
+    result_set.append(state.as_dict())
+    return result_set
 
+class LocalGovernmentArea(Object):
+
+  def as_dict(self):
+    lga = {}
+    lga["name"] = self.name
+    return lga
+
+  @staticmethod
+  def get_all_lgas_with_state_name(state_name):
+    result_set = []
+    state = State.Query.get(name = state_name).limit(1)
+    lgas = LocalGovernmentArea.Query.get(state = state.objectId)
+    for LGA in lgas:
+        result_set.append(LGA.as_dict())
+    return result_set
