647f3fcc3a4eb9fd6fb97269ec926c4839a6c09f
Timi <timi@helloworld.ng>
2015-11-30 19:23:13 +0100

    fixed current endpoints and changed some names

--- app/mod_endpoints/controllers.py
+++ app/mod_endpoints/controllers.py
@@ -2,7 +2,7 @@ from flask import Blueprint, Response
 from json import dumps
 # Import module models
 from app.mod_endpoints.models import State
-from app.mod_endpoints.models import LocalGovernmentArea
+from app.mod_endpoints.models import LGA
 
 # Define the blueprint: 'endpoints', set its url prefix: app.url/api/v1
 mod_endpoints = Blueprint('api/v1', __name__, url_prefix='/api/v1')
@@ -15,18 +15,6 @@ def get_states():
     return Response(dumps(states), mimetype='application/json')
 
 
-@mod_endpoints.route('/<state_name>/lgas', methods=['GET'])
-def get_lgas(state_name):
-    lgas = LocalGovernmentArea.get_all_lgas_with_state_name(state_name)
-    return Response(dumps(lgas), mimetype='application/json')
-
-
-@mod_endpoints.route('/<state_code>/lgas', methods=['GET'])
-def get_lgas_with_state_code(state_code):
-    lgas = LocalGovernmentArea.get_all_lgas_with_state_code(state_code)
-    return Response(dumps(lgas), mimetype='application/json')
-
-
 @mod_endpoints.route('/state_with_code/<state_code>', methods=['GET'])
 def get_state_with_state_code(state_code):
     state = State.get_one_state_with_state_code(state_code.upper())
@@ -37,3 +25,18 @@ def get_state_with_state_code(state_code):
 def get_state(state_name):
     state = State.get_one_state(state_name.capitalize())
     return Response(dumps(state), mimetype='application/json')
+
+
+@mod_endpoints.route('/lgas_with_state_name/<state_name>/', methods=['GET'])
+def get_lgas(state_name):
+    lgas = LGA.get_all_lgas_with_state_name(state_name.capitalize())
+    return Response(dumps(lgas), mimetype='application/json')
+
+
+@mod_endpoints.route('/lgas_with_state_code/<state_code>/', methods=['GET'])
+def get_lgas_with_state_code(state_code):
+    lgas = LGA.get_all_lgas_with_state_code(state_code.upper())
+    return Response(dumps(lgas), mimetype='application/json')
+
+
+
--- app/mod_endpoints/models.py
+++ app/mod_endpoints/models.py
@@ -35,7 +35,7 @@ class State(Object):
         return state.as_dict()
 
 
-class LocalGovernmentArea(Object):
+class LGA(Object):
     def as_dict(self):
         lga = {}
         lga["name"] = self.name
@@ -44,29 +44,18 @@ class LocalGovernmentArea(Object):
     @staticmethod
     def get_all_lgas_with_state_name(state_name):
         result_set = []
-        state = State.Query.get(name=state_name).limit(1)
-        lgas = LocalGovernmentArea.Query.get(state=state.objectId)
-        for LGA in lgas:
-            result_set.append(LGA.as_dict())
+        state_result = State.Query.get(name=state_name)
+        print state_result.objectId
+        lgas = LGA.Query.filter(state=state_result)
+        for lga in lgas:
+            result_set.append(lga.as_dict())
         return result_set
 
     @staticmethod
     def get_all_lgas_with_state_code(code):
         result_set = []
-        state = State.Query.get(state_code=code).limit(1)
-
-
-class LocalGovernmentArea(Object):
-    def as_dict(self):
-        lga = {}
-        lga["name"] = self.name
-        return lga
-
-    @staticmethod
-    def get_all_lgas_with_state_name(state_name):
-        result_set = []
-        state = State.Query.get(name=state_name).limit(1)
-        lgas = LocalGovernmentArea.Query.get(state=state.objectId)
-        for LGA in lgas:
-            result_set.append(LGA.as_dict())
+        state_result = State.Query.get(state_code=code)
+        lgas = LGA.Query.filter(state=state_result)
+        for lga in lgas:
+            result_set.append(lga.as_dict())
         return result_set
